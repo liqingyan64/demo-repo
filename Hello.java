4715a18df8aa254109f626a4edfbaf2008e270c5
liliqingyan64 <liliqingyan@gmail.com>
2025-08-04 14:45:20 +0800

    Creat  Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello the world!");
+        System.out.println("Welcome to the git world!");
+    }
+}
